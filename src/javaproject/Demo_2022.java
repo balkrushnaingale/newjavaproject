f79eb6073a0810aecc1b892b04efe4b883bedb9d
balkrushnaingale <107671821+balkrushnaingale@users.noreply.github.com>
2022-06-20 16:18:39 +0530

    updated project

--- src/javaproject/Demo_2022.java
+++ src/javaproject/Demo_2022.java
@@ -4,7 +4,7 @@ public class Demo_2022 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is java project");
+System.out.println("This is Spring project");
 	}
 
 }
