433a58d17e20cc785efcb1a7b3463d7b67a81c00
balkrushnaingale <balkrushna6321@gmail.com>
2022-06-20 16:15:24 +0530

    this is javaproject

---
+++ src/javaproject/Demo_2022.java
@@ -0,0 +1,10 @@
+package javaproject;
+
+public class Demo_2022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is java project");
+	}
+
+}
